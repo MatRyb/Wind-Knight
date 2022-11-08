9536b7a496f50f10d0e5e93729db0336fca77d7d
MatRyb <matti-2002@wp.pl>
2022-11-08 15:49:53 +0100

    Update EnemyRanged.cs

--- Assets/Scripts/EnemyRanged.cs
+++ Assets/Scripts/EnemyRanged.cs
@@ -29,7 +29,7 @@ public class EnemyRanged : EnemyController
 
         if(distanceToPlayer <= range)
         {
-            if (canShoot == true)
+            if (canShoot)
             {
                 Attack();
             }
