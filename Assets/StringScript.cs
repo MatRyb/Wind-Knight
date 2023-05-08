ffc4162c4c79026f83087e637bd0f87232c48545
MatRyb <matti-2002@wp.pl>
2023-05-08 20:53:31 +0200

    string fixes and updates

--- Assets/StringScript.cs
+++ Assets/StringScript.cs
@@ -14,12 +14,20 @@ public class StringScript : MonoBehaviour
         {
             aboveSegment.connectedBelow = gameObject;
             float spriteBottom = connectedAbove.GetComponent<SpriteRenderer>().bounds.size.y;
-            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, spriteBottom * -1);
-
+            //GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, spriteBottom * -7);
+            GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0,-5);
         }
         else
         {
             GetComponent<HingeJoint2D>().connectedAnchor = new Vector2(0, 0);
         }
+        StartCoroutine(waiter());
+    }
+
+    private IEnumerator waiter()
+    {
+        yield return new WaitForSeconds(3);
+        gameObject.GetComponent<BoxCollider2D>().isTrigger = false;
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 1f;
     }
 }
