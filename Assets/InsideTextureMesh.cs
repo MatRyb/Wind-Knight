068808d41d91134f0e15f2532041d644b1aa21bf
MatRyb <matti-2002@wp.pl>
2023-06-22 10:01:23 +0200

    More Txt and Started Mesh

---
+++ Assets/InsideTextureMesh.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InsideTextureMesh : MonoBehaviour
+{   /*
+    Vector3[] Verticles;
+    private Mesh mesh;
+    private MeshFilter meshFilter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Verticles = gameObject.GetComponentInChildren(Transform);
+    } */
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
